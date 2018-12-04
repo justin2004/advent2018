ed1e9c16ff77a945ff52daee768f3077182479d8
justin <justin2004@hotmail.com>
2018-12-03 21:52:58 -0500

    day 2 part 1 working

--- day2/a.lisp
+++ day2/a.lisp
@@ -1,37 +1,65 @@
 #!/usr/bin/sbcl --script
 
-;(setf f (open "/home/justin/aoc_2018/day2/input/data"))
-(setf f '("abcdef" "bababc" "abbcde" "abcccd" "aabcdd" "abcdee" "ababab")
+(do_it (read_file_into_seq_of_strings "/home/justin/aoc_2018/day2/input/data"))
 
-(setf ht (make-hash-table))
 
-(setf (gethash #\a ht) 1) 
+(defun read_file_into_seq_of_strings (filename)
+  (with-open-file (f filename :direction :input)
+  (defun getlines(current_seq_of_lines)
+        (let ((line (read-line f nil 'done nil)))
+          (if (eq line 'done)
+              current_seq_of_lines
+              (getlines (append current_seq_of_lines (list line))))))
+  (getlines '())))
 
-(map 'list #'identity "abcde")
-(mapcar #'(lambda (x) x) (map 'list #'identity "abcde"))
+(append '(a b) (list 1) (list 5 6))
+  
+
+              
+(setf f '("abcdef" "bababc" "abbcde" "abcccd" "aabcdd" "abcdee" "ababab"))
+
+(map 'list #'identity "abcde") ; turn string to list of chars
 
 (histo (map 'list #'identity "aaabcdebe"))
-(justin  *)
-(maphash #'(lambda (key val) (format t "~A:~A~%" key val)) ht)
+(maphash #'(lambda (k v) (format t "~A:~A~%" k v)) (histo (map 'list #'identity "aaabcdebe")))
 
-(setf acc '())
-acc
-(append '(1 2) (list 55))
+(checksum "justinpj")
+
+(defun checksum (string)
+  (eval `(* ,@(ht_to_dub_trip_list (histo (map 'list #'identity string))))))
+
+(mapcar #'(lambda (string) (ht_to_dub_trip_list (histo (map 'list #'identity string))))
+        f)
+
+(setf ff *)
+ff
+
+(do_it f)
+
+(defun do_it (seq_of_strings)
+  (let ((doubles_total 0) (triples_total 0))
+    (mapcar #'(lambda (pair) (setf doubles_total (+ doubles_total (car pair))) (setf triples_total (+ triples_total (cadr pair))))
+            (mapcar #'(lambda (string) (ht_to_dub_trip_list (histo (map 'list #'identity string))))
+                    seq_of_strings))
+    (* doubles_total triples_total)))
 
 
-(defun justin (ht) 
-  "returns a list with number of double and number of triples"
+(defun ht_to_dub_trip_list (ht) 
+  "returns a list with number of double and number of triples ... but with the comment below"
   (let ((two_count 0) (three_count 0))
     (maphash #'(lambda (key val) 
-                 (if (= val 2)
+                 (if (and (= val 2)
+                          (= two_count 0)) ; X doubles still just mean 1
                      (setf two_count   (+ two_count 1)))
-                 (if (= val 3)
+                 (if (and (= val 3)
+                          (= three_count 0)) ; X triples still just mean 1
                      (setf three_count (+ three_count 1))))
              ht)
     (list two_count three_count)))
 
   
 (defun histo (seq)
+  "create a histogram for the sequence"
   (let ((ht (make-hash-table)))
   (mapcar #'(lambda (element)
               (multiple-value-bind (value anything?) (gethash element ht) 
